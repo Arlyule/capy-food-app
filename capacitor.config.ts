6ad5b8db361e95f73f955251fc0d153bd4af88f6
Arlyule <torresreyessamel2@gmail.com>
2024-12-06 20:14:53 -0600

    ultimos cambios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.templates',
-  appName: 'ionic_ui_templates',
+  appName: 'capy_food_app',
   webDir: 'www',
   server: {
     androidScheme: 'https'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4110',
+  apiUrl: 'https://capy-food-backend.onrender.com',
 };
